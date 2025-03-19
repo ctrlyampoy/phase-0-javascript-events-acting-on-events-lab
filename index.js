72f42a0bf8316a02ac9ae7460fd3884557d44479
Yampoy <yampoy.elvis@gmail.com>
2025-03-19 08:37:10 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+
+dodger.style.backgroundColor = "#000000"; 
+
+dodger.style.backgroundColor = "#FF69B4"; 
+
+dodger.style.bottom  = "100px";
+
+dodger.style.bottom  = "0px";
+
+dodger.style.left  = "0px";
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === "ArrowLeft") {
+        moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+        moveDodgerRight();
+    }
+});
+
+function moveDodgerLeft() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left.replace('px', ''), 10);
+
+    if (left > 0) {
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const dodger = document.getElementById('dodger');
+    let left = parseInt(dodger.style.left.replace('px', ''), 10);
+
+    if (left < (window.innerWidth - dodger.offsetWidth)) {
+        dodger.style.left = `${left + 1}px`;
+    }
+}
